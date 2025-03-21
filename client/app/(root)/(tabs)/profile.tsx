85cb41f10af0f1311580ad0a9ed23e5cf003d0ab
hieuphung41 <phungduchieu6996@gmail.com>
2025-03-21 17:29:49 +0700

    Fix ui for profile

--- client/app/(root)/(tabs)/profile.tsx
+++ client/app/(root)/(tabs)/profile.tsx
@@ -4,7 +4,6 @@ import {
   ScrollView,
   Image,
   TouchableOpacity,
-  ImageSourcePropType,
   Alert,
 } from "react-native";
 import React from "react";
@@ -18,48 +17,27 @@ import { RootState } from "@/redux/store";
 import { useSignOutMutation } from "@/redux/api/authApiSlice";
 
 interface SettingsItemProp {
-  icon: ImageSourcePropType;
   title: string;
-  onPress?: () => void;
   textStyle?: string;
-  showArrow?: boolean;
 }
 
-const SettingsItem = ({
-  icon,
-  title,
-  onPress,
-  textStyle,
-  showArrow = true,
-}: SettingsItemProp) => (
-  <TouchableOpacity
-    onPress={onPress}
-    className="flex flex-row items-center justify-between py-3"
-  >
-    <View className="flex flex-row items-center gap-3">
-      <Image source={icon} className="size-6" />
-      <Text className={`text-lg font-rubik-medium text-black-300 ${textStyle}`}>
-        {title}
-      </Text>
-    </View>
-
-    {showArrow && <Image source={icons.rightArrow} className="size-5" />}
-  </TouchableOpacity>
+const SettingsItem = ({ title, textStyle }: SettingsItemProp) => (
+  <View className="flex flex-row items-center justify-between py-3">
+    <Text className={`text-lg font-rubik-medium text-black-300 ${textStyle}`}>
+      {title}
+    </Text>
+  </View>
 );
 
 const Profile = () => {
   const router = useRouter();
   const userInfo = useSelector((state: RootState) => state.auth.user);
 
-  console.log(userInfo);
   const userId = userInfo?.user?._id;
 
   // Gọi API lấy thông tin người dùng
   const { data, isLoading, error } = useGetUserQuery(userId);
   const user = data?.user;
-  console.log("====================================");
-  console.log(data);
-  console.log("====================================");
 
   // API logout
   const [logout] = useSignOutMutation();
@@ -82,24 +60,13 @@ const Profile = () => {
   }
 
   if (error) {
-    console.log("====================================");
-    console.log(error);
-    console.log("====================================");
     return (
       <View className="flex-1 justify-center items-center">
         <Text className="text-red-500">Error loading profile</Text>
       </View>
     );
   }
-  const navigateToCart = () => {
-    router.push("/cart");
-  };
-  const navigateToUserDetail = () => {
-    router.push("/user");
-  };
-  const navigateToOrder = () => {
-    router.push("/order");
-  };
+
   return (
     <SafeAreaView className="h-full bg-white">
       <ScrollView
@@ -108,18 +75,11 @@ const Profile = () => {
       >
         <View className="flex flex-row items-center justify-between mt-5">
           <Text className="text-xl font-rubik-bold">Profile</Text>
-          <TouchableOpacity onPress={() => router.navigate("/cart")}>
-            <Image source={icons.cart} className="size-6" />
-          </TouchableOpacity>
         </View>
 
         <View className="flex flex-row justify-center mt-5">
           <View className="flex flex-col items-center relative mt-5">
             <Image source={avatar} className="size-44 relative rounded-full" />
-            <TouchableOpacity className="absolute bottom-11 right-2">
-              <Image source={icons.edit} className="size-9" />
-            </TouchableOpacity>
-
             <Text className="text-2xl font-rubik-bold mt-2">
               {user?.name || "Unknown User"}
             </Text>
@@ -127,36 +87,15 @@ const Profile = () => {
         </View>
 
         <View className="flex flex-col mt-10">
-          {/* <SettingsItem icon={icons.cart} title="My Cart" onPress={navigateToCart}/> */}
-          <SettingsItem
-            icon={icons.bell}
-            title="Notifications"
-            onPress={() => {}}
-          />
-          <SettingsItem
-            icon={icons.calendar}
-            title="My Orders"
-            onPress={navigateToOrder}
-          />
-          <SettingsItem icon={icons.wallet} title="Payments" />
-        </View>
-
-        <View className="flex flex-col mt-5 border-t pt-5 border-primary-200">
-          <SettingsItem
-            icon={icons.person}
-            title="Profile"
-            onPress={navigateToUserDetail}
-          />
-          <SettingsItem icon={icons.language} title="Language" />
-          <SettingsItem icon={icons.info} title="Help Center" />
+          <SettingsItem title={`Name: ${user?.name || "Unknown"}`} />
+          <SettingsItem title={`Email: ${user?.email || "Unknown"}`} />
+          <SettingsItem title={`Phone: ${user?.phone || "Unknown"}`} />
         </View>
 
         <View className="flex flex-col border-t mt-5 pt-5 border-primary-200">
           <SettingsItem
-            icon={icons.logout}
             title="Logout"
             textStyle="text-danger"
-            showArrow={false}
             onPress={handleLogout}
           />
         </View>
